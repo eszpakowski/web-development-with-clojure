35d48f4e1e841280231f7a5cce915408bd45e043
Emil Szpakowski <emil.szpakowski@gmail.com>
2021-02-02 21:05:16 +0100

    Add new endpoint and improve home template

--- apps/guestbook/src/clj/guestbook/routes/home.clj
+++ apps/guestbook/src/clj/guestbook/routes/home.clj
@@ -1,14 +1,19 @@
 (ns guestbook.routes.home
   (:require
-   [guestbook.layout :as layout]
-   [guestbook.db.core :as db]
-   [clojure.java.io :as io]
-   [guestbook.middleware :as middleware]
-   [ring.util.response]
-   [ring.util.http-response :as response]))
+    [guestbook.layout :as layout]
+    [guestbook.db.core :as db]
+    [clojure.java.io :as io]
+    [guestbook.middleware :as middleware]
+    [ring.util.response]
+    [ring.util.http-response :as response]))
 
 (defn home-page [request]
-  (layout/render request "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
+  (layout/render
+    request "home.html" {:messages (db/get-messages)}))
+
+(defn save-message! [{:keys [params]}]
+  (db/save-message! params)
+  (response/found "/"))
 
 (defn about-page [request]
   (layout/render request "about.html"))
@@ -18,5 +23,6 @@
    {:middleware [middleware/wrap-csrf
                  middleware/wrap-formats]}
    ["/" {:get home-page}]
+   ["/message" {:post save-message!}]
    ["/about" {:get about-page}]])
 
