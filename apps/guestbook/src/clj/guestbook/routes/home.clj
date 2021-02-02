03ddef2f9d2e0205e4ede3d7b103c63e6fd1b3c0
Emil Szpakowski <emil.szpakowski@gmail.com>
2021-02-02 21:54:45 +0100

    Add simple validation

--- apps/guestbook/src/clj/guestbook/routes/home.clj
+++ apps/guestbook/src/clj/guestbook/routes/home.clj
@@ -5,15 +5,36 @@
     [clojure.java.io :as io]
     [guestbook.middleware :as middleware]
     [ring.util.response]
-    [ring.util.http-response :as response]))
+    [ring.util.http-response :as response]
+    [struct.core :as struct]))
 
-(defn home-page [request]
+(def message-schema
+  [[:name
+    struct/required
+    struct/string]
+   [:message
+    struct/required
+    struct/string
+    {:message  "Message must have at least 10 characters!"
+     :validate (fn [msg] (>= (count msg) 10))}]])
+
+(defn validate-message [params]
+  (first (struct/validate params message-schema)))
+
+(defn home-page [{:keys [flash] :as request}]
   (layout/render
-    request "home.html" {:messages (db/get-messages)}))
+    request
+    "home.html"
+    (merge {:messages (db/get-messages)}
+           (select-keys flash [:name :message :errors]))))
 
 (defn save-message! [{:keys [params]}]
-  (db/save-message! params)
-  (response/found "/"))
+  (if-let [errors (validate-message params)]
+    (-> (response/found "/")
+        (assoc :flash (assoc params :errors errors)))
+    (do
+      (db/save-message! params)
+      (response/found "/"))))
 
 (defn about-page [request]
   (layout/render request "about.html"))
