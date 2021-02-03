cf2f22d0cc4e42bf723308dabc11728fe4f8c003
Emil Szpakowski <emil.szpakowski@gmail.com>
2021-02-03 19:45:04 +0100

    Add sample ring handler

--- apps/ring-app/src/ring_app/core.clj
+++ apps/ring-app/src/ring_app/core.clj
@@ -1,6 +1,13 @@
-(ns ring-app.core)
+(ns ring-app.core
+  (:require [ring.adapter.jetty :as jetty]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn handler [request-map]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    (str "<html><body>Your IP is: "
+                 (:remote-addr request-map) "</body></html>")})
+
+(defn -main []
+  (jetty/run-jetty
+    handler
+    {:port 3000 :join? false}))
