330f7e4afdf41c036163d65c4705b38d7a32cf94
Emil Szpakowski <emil.szpakowski@gmail.com>
2021-02-03 21:04:49 +0100

    Add muuntaja wrapper

--- apps/ring-app/project.clj
+++ apps/ring-app/project.clj
@@ -5,6 +5,7 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [ring "1.7.1"]
-                 [metosin/ring-http-response "0.9.1"]]
+                 [metosin/ring-http-response "0.9.1"]
+                 [metosin/muuntaja "0.6.4"]]
   :repl-options {:init-ns ring-app.core}
   :main ring-app.core)
--- apps/ring-app/src/ring_app/core.clj
+++ apps/ring-app/src/ring_app/core.clj
@@ -1,13 +1,20 @@
 (ns ring-app.core
   (:require [ring.adapter.jetty :as jetty]
             [ring.util.http-response :as response]
-            [ring.middleware.reload :refer [wrap-reload]]))
+            [ring.middleware.reload :refer [wrap-reload]]
+            [muuntaja.middleware :as muuntaja]))
 
-(defn handler [request-map]
+(defn html-handler [request-map]
   (response/ok
     (str "<html><body>Hello, your IP is: "
          (:remote-addr request-map)
          "</body></html>")))
+
+(defn json-handler [request-map]
+  (response/ok
+    {:result (get-in request-map [:body-params :id])}))
+
+(def handler json-handler)
 
 (defn wrap-nocache [handler]
   (fn [request-map]
@@ -15,10 +22,15 @@
         handler
         (assoc-in [:headers "Pragma"] "no-cache"))))
 
+(defn wrap-formats [handler]
+  (-> handler
+      (muuntaja/wrap-format)))
+
 (defn -main []
   (jetty/run-jetty
     (-> #'handler
         wrap-nocache
+        wrap-formats
         wrap-reload)
-    {:port 3000
+    {:port  3000
      :join? false}))
