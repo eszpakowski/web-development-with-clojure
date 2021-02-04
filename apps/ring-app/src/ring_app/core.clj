87e7b5be8b319a6eff29bcb31ce69d9042c53928
Emil Szpakowski <emil.szpakowski@gmail.com>
2021-02-04 21:24:22 +0100

    Add default reitit handler

--- apps/ring-app/src/ring_app/core.clj
+++ apps/ring-app/src/ring_app/core.clj
@@ -43,8 +43,16 @@
          {:values (map (apply juxt [:a :b]) params)}))}]])
 
 (def handler
-  (reitit/ring-handler
-    (reitit/router routes)))
+  (reitit/routes
+    (reitit/ring-handler
+      (reitit/router routes))
+    (reitit/create-default-handler
+      {:not-found
+       (constantly (response/not-found "404 - Page not found"))
+       :method-not-allowed
+       (constantly (response/method-not-allowed "405 - Not allowed"))
+       :not-acceptable
+       (constantly (response/not-acceptable "406 - Not acceptable"))})))
 
 (defn -main []
   (jetty/run-jetty
